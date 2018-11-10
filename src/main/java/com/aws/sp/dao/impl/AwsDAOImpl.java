b455c717e4211c39bf2dd7375560cec72711ed42
rlaehdgur45 <rlaehdgur45@naver.com>
2018-11-10 15:27:56 +0900

    asdf

--- src/main/java/com/aws/sp/dao/impl/AwsDAOImpl.java
+++ src/main/java/com/aws/sp/dao/impl/AwsDAOImpl.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.aws.sp.dao.AwsDAO;
 import com.aws.sp.vo.AwsVO;
